ef4736dec1201ad80af5c85687e0efb8592c0d04
ianhorn <ianhorn17@gmail.com>
2026-07-18 21:41:39 -0400

    add stac search to script
    create a requirements.txt file
    
    reconfigure creating a buffered bbox

--- src/create_buffered_tile.py
+++ src/create_buffered_tile.py
@@ -6,60 +6,93 @@ specfied distance, uses the bufferred bounding box to search stac for lidar tile
 The points from each lidar will be read to create a new temporary local tile. 
 """
 
-
+import json
+import requests
 from pystac import Item
 from pathlib import Path
-from shapely.geometry import shape
 from pystac_client import ItemSearch
 
 
+def get_stac_item(item_id: str, item_collection: str, item_api_url: str):
+    """
+    this function uses pystac to grab the item from file (href)
+    """
+    
+    href = f'{item_api_url}/collections/{item_collection}/items/{item_id}'
+    stac_item = Item.from_file(href)
+    return stac_item
+
+
 def get_distance_degrees(meters: float):
+    """
+    This functions calculates the buffer distance in meters
+    and converts to degrees because we are working with 
+    WGS84.
+    """
+
     meters_per_degree = 111_320
     distance_meters = meters
     distance_degrees = distance_meters / meters_per_degree
-
     return distance_degrees
 
-def get_buffered_geometry(item_id: str, item_collection: str, item_api_url: str, distance_degrees: float):
+
+def get_buffered_bbox(item_bbox, distance_degrees):
     """
-    requires the STAC API url, stac title
+    We need to create a buffered bbox so we can later 
+    query the stac to include surrounding tiles.
     """
 
-    href = f'{item_api_url}/collections/{item_collection}/items/{item_id}'
+    minx, miny, maxx, maxy = item_bbox
 
-    item = Item.from_file(href)  # pull the item with pystac
-    geometry = item.geometry    # get the geometry
-    print(f'Geometry: {geometry}\n')                  # print for confirmation
-
-    # buffer the bbox by a distance
-    buffered_geometry = shape(item.geometry).buffer(
-        distance_degrees,
-        cap_style="square"
-    )
-    print(f'Buffer: {buffered_geometry}\n')                   # print for confirmation
-
-    return buffered_geometry
+    buffered_bbox = [
+        minx - distance_degrees,
+        miny - distance_degrees,
+        maxx - distance_degrees,
+        maxy - distance_degrees
+    ]
+    return buffered_bbox
 
 
-def search_stac(buffered_geometry):
+def search_stac(stac_api, collection, buffered_bbox):
 
-    geometry = get_buffered_geometry(item, collection, stac, distance)
-#     stac_string = f'{stac}/'
+    url = f'{stac_api}/search'
 
-    return geometry
+    payload = {
+        "collections": [collection],
+        "bbox": buffered_bbox
+    }
+
+    headers = {
+        "accept": "application/geo+json",
+        "Content-Type": "application/json",
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+    response.raise_for_status()
+
+    data = response.json()
+    print(f'Response Status: {response.status_code}')
+    # print(json.dumps(data, indent=2))
 
 
 def main():
 
-    item = 'N075E299_LAS_Phase2.copc'
+    item_id = 'N075E299_LAS_Phase2.copc'
     collection = 'laz-phase2'
     stac = 'https://spved5ihrl.execute-api.us-west-2.amazonaws.com'
 
+    item = get_stac_item(item_id, collection, stac)
+    bbox = item.bbox
+    print(f'bbox {bbox}')
     buffer = 30
     distance = get_distance_degrees(buffer)
-    buffered_geometry = get_buffered_geometry(item, collection, stac, distance)
+    print(f'Degrees: {distance}')
+    bbox_buffer = get_buffered_bbox(item.bbox, distance)
+    print(f'Buffered bbox: {bbox_buffer}')
 
     print(f'Item: {item}\n')
+
+    stac_search = search_stac(stac, collection, bbox_buffer)
 
 if __name__ == '__main__':
     main()
