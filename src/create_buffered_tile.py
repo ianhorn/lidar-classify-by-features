ce168340f0ceb9b03a105c3fa9a6fdd3a89517f5
ianhorn <ianhorn17@gmail.com>
2026-07-20 23:10:42 -0400

    add pdal pipeline

--- src/create_buffered_tile.py
+++ src/create_buffered_tile.py
@@ -6,10 +6,14 @@ specfied distance, uses the bufferred bounding box to search stac for lidar tile
 The points from each lidar will be read to create a new temporary local tile. 
 """
 
+
 import json
+import pdal
+import time
 import requests
 from pystac import Item
 from pathlib import Path
+from pyproj import Transformer
 from pystac_client import Client
 
 
@@ -53,7 +57,35 @@ def get_buffered_bbox(item_bbox, distance_degrees):
     return buffered_bbox
 
 
-def search_stac(stac_api, collection, buffered_bbox):
+def reproject_bbox(bbox, src_epsg=4326, dst_epsg=3089):
+    """
+    Reproject a WGS84 bbox to Kentucky Single Zone (EPSG:3089).
+    """
+
+    transformer = Transformer.from_crs(
+        src_epsg,
+        dst_epsg,
+        always_xy=True
+    )
+
+    minx, miny, maxx, maxy = bbox
+
+    x1, y1 = transformer.transform(minx, miny)
+    x2, y2 = transformer.transform(maxx, maxy)
+
+    return [
+        min(x1, x2),
+        min(y1, y2),
+        max(x1, x2),
+        max(y1, y2)
+    ]
+
+
+def pdal_bounds(bbox):
+    xmin, ymin, xmax, ymax = bbox
+    return f"([{xmin},{xmax}],[{ymin},{ymax}])"
+
+def search_stac(stac_api: str, collection: str, buffered_bbox):
     """
     Use pystac_client to open the stac api
     search by bbox
@@ -70,7 +102,7 @@ def search_stac(stac_api, collection, buffered_bbox):
     print(f'Found {len(list(search.items()))} items\n')
     item_list = list(search.items())
     for i in item_list:
-        print(i.id)
+        print(i)
 
     href_list = []
 
@@ -79,20 +111,45 @@ def search_stac(stac_api, collection, buffered_bbox):
             href_list.append(asset.href)
     for h in href_list:
         print(h)
-
+        
     return href_list
 
-# def process_copc(copc):
 
-#     json = {
+def crop_copc(hrefs, bounds, out_laz):
 
-#     }
+    pipeline = []
 
+    for href in hrefs:
+        pipeline.append(
+            {
+                "type": "readers.copc",
+                "filename": href,
+                "bounds": bounds
+            }
+        )
 
+    pipeline.append(
+        {
+            "type": "writers.las",
+            "filename": str(out_laz)
+        }
+    )
+
+    print(json.dumps(pipeline, indent=2))
+
+    p = pdal.Pipeline(json.dumps(pipeline))
+
+    start = time.perf_counter()
+    count = p.execute()
+    elapsed = time.perf_counter() - start
+
+    print(f"{count:,} points written")
+    print(f"Elapsed: {elapsed:.1f} seconds")
 
 def main():
 
     item_id = 'N075E299_LAS_Phase2.copc'
+    out_laz = Path('/mnt/d/Data/lidar/N075E299.laz')
     # item_id = 'N075E295_LAS_Phase2.copc'
     collection = 'laz-phase2'
     stac = 'https://spved5ihrl.execute-api.us-west-2.amazonaws.com'
@@ -108,7 +165,18 @@ def main():
     bbox_buffer = get_buffered_bbox(item.bbox, distance)
     print(f'Buffered bbox: {bbox_buffer}')
 
-    stac_search = search_stac(stac, collection, bbox_buffer)
+
+    hrefs = search_stac(stac, collection, bbox_buffer)
+    # print(stac_search)
+    for h in hrefs:
+        print(h)
+
+    bbox_3089 = reproject_bbox(bbox_buffer)
+
+    print(f"3089 bbox: {bbox_3089}")
+
+    bounds = pdal_bounds(bbox_3089)
+    crop_copc(hrefs, bounds, out_laz)
 
 
 if __name__ == '__main__':
